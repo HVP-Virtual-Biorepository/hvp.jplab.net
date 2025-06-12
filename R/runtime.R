816dfd7aec75280fa2e2d06739718d457ae043ca
Daniel Smith <dansmith@orst.edu>
2025-06-12 17:30:01 +0100

    as_stringified_json

--- R/runtime.R
+++ R/runtime.R
@@ -1,5 +1,11 @@
 handler <- function(context) {
-  return(jsonlite::toJSON(context))
+  lambdr::as_stringified_json(
+    list(
+      isBase64Encoded = FALSE,
+      statusCode = 200L,
+      body = lambdr::as_stringified_json(context)
+    )
+  )
 }
 
 lambdr::start_lambda()

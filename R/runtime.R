3099ec7d92432255497ce65eeb07f2ae18cf8fb1
Daniel Smith <dansmith@orst.edu>
2025-06-12 17:32:27 +0100

    no encode

--- R/runtime.R
+++ R/runtime.R
@@ -1,11 +1,5 @@
 handler <- function(context) {
-  lambdr::as_stringified_json(
-    list(
-      isBase64Encoded = FALSE,
-      statusCode = 200L,
-      body = lambdr::as_stringified_json(context)
-    )
-  )
+  context
 }
 
 lambdr::start_lambda()

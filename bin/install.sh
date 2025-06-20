10a09e7ca39c6f7210b5b1fc339fa71b626cbee7
Daniel Smith <dansmith@orst.edu>
2025-06-20 22:51:46 +0100

    git pre-installed

--- bin/install.sh
+++ bin/install.sh
@@ -2,7 +2,7 @@
 apt-get update
 
 ln -fs /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
-apt-get install -y git apache2 libapache2-mod-r-base mysql-server libcurl4-openssl-dev
+apt-get install -y apache2 libapache2-mod-r-base mysql-server libcurl4-openssl-dev
 
 a2dissite 000-default
 a2enmod md
